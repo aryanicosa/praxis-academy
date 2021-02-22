4c4f92ccaebcae693d373facbff9fa29a7de7264
aryanicosa <aryanicosa@gmail.com>
2021-02-22 11:15:05 +0700

    memindahkan file yang tidak dibutuhkan

--- rp_portofolio2/awesome_website/users/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import include, url
-from users.views import dashboard
-
-urlpatterns = [
-    url(r'^dashboard/', dashboard, name='dashboard'),
-    url(r'^accounts/', include('django.contrib.auth.urls')),
-]
